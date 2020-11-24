8a3c25aeebf59ef72a78e280a461ce70c4f91b45
allenwind <allenwind@foxmail.com>
2020-11-24 21:59:33 +0800

    Add MaskBiLSTM

--- layers_with_mask.py
+++ layers_with_mask.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.keras.layers import *
 
 class MaskGlobalAveragePooling1D(tf.keras.layers.Layer):
     
@@ -91,3 +92,29 @@ class AttentionPooling1D(tf.keras.layers.Layer):
         # 加权平均
         x = tf.reduce_sum(x * x0, axis=1)
         return x
+
+class MaskBiLSTM(tf.keras.layers.Layer):
+    def __init__(self, hdims, **kwargs):
+        super(MaskBiLSTM, self).__init__(**kwargs)
+        self.hdims = hdims
+        self.forward_lstm = LSTM(hdims, return_sequences=True)
+        self.backend_lstm = LSTM(hdims, return_sequences=True)
+
+    def reverse_sequence(self, x, mask):
+        seq_len = tf.reduce_sum(mask, axis=1)[:, 0]
+        seq_len = tf.cast(seq_len, tf.int32)
+        x = tf.reverse_sequence(x, seq_len, seq_axis=1)
+        return x
+
+    def call(self, inputs, mask):
+        x = inputs
+        x_forward = self.forward_lstm(x)
+        x_backward = self.reverse_sequence(x, mask)
+        x_backward = self.backend_lstm(x_backward)
+        x_backward = self.reverse_sequence(x_backward, mask)
+        x = tf.concat([x_forward, x_backward], axis=-1)
+        x = x * mask
+        return x
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0][:-1] + (self.hdims * 2,)
